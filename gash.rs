3007f843b931196e74293b4ded74b39105f65b79
morpheus <aqy8je@virginia.edu>
2013-09-22 16:25:08 -0400

    First commit; basic structure.

--- gash.rs
+++ gash.rs
@@ -2,7 +2,9 @@ use std::{io, run};
 
 fn main() {
     static CMD_PROMPT: &'static str = "gash > ";
-    
+    let mut x = 0;
+    let mut hist = std::vec::from_elem(100,~"test");	
+
     loop {
         print(CMD_PROMPT);
         let line = io::stdin().read_line();
@@ -15,8 +17,14 @@ fn main() {
             let program = argv.remove(0);
             match program {
                 ~"exit"     => {return; }
+		~"history"  => {
+			println(hist[0]);
+		}
+		~"cd"	    => {println("RUST");}
                 _           => {run::process_status(program, argv);}
             }
+	    // Implement history list here.
+	    
         }
     }
 }

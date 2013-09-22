4ae34fecdcf0a43aa6ec87b1b292251aa0a49b62
morpheus <aqy8je@virginia.edu>
2013-09-22 17:54:33 -0400

    Hackiest solution in the world, but history works. Will not revisit this unless under penalty of castration.

--- gash.rs
+++ gash.rs
@@ -3,17 +3,27 @@ use std::{io, run};
 fn main() {
     static CMD_PROMPT: &'static str = "gash > ";
     let mut ct = 0;
-    let mut hist = std::vec::from_elem(100,~"test");	
+    let mut size = 2;
+    let mut hist = ~std::vec::from_elem(size,~"test");	
 
     loop {
         print(CMD_PROMPT);
         let line = io::stdin().read_line();
         debug!(fmt!("line: %?", line));
+	
+	// Fringe case; history buffer is at capacity. Double size.
+	if (ct == size - 2) {
+		let temp = ~std::vec::from_elem(size,~"test");
+		let fin  = ~std::vec::concat([(*hist),(*temp)]);
+		size = size * 2;
+		hist = fin;
+	}		
 
 	// Adding the line to history.
-	hist[ct] = std::str::to_owned(line);
+	(*hist)[ct] = std::str::to_owned(line);
 	ct += 1;
 
+	// Read in the "command" in the shell.
         let mut argv: ~[~str] = line.split_iter(' ').filter(|&x| x != "")
                                  .transform(|x| x.to_owned()).collect();
         debug!(fmt!("argv %?", argv));
@@ -21,10 +31,11 @@ fn main() {
         if argv.len() > 0 {
             let program = argv.remove(0);
             match program {
+		// Internal command implementations here.
                 ~"exit"     => {return; }
 		~"history"  => {
 			for std::uint::range(0,ct) |i| {
-				println(fmt!("%s  %s",std::uint::to_str(i+1),hist[i]));
+				println(fmt!("%s  %s",std::uint::to_str(i+1),(*hist)[i]));
 			}
 	        }
 		~"cd"	    => {println("RUST");}

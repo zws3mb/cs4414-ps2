5e4a766e928350cb9d30245be78c33c3d0fcad7c
morpheus <aqy8je@virginia.edu>
2013-09-22 16:53:15 -0400

    Rudimentary history function implemented. > 100 commands = death for now.

--- gash.rs
+++ gash.rs
@@ -2,15 +2,18 @@ use std::{io, run};
 
 fn main() {
     static CMD_PROMPT: &'static str = "gash > ";
-    let mut x = 0;
+    let mut ct = 0;
     let mut hist = std::vec::from_elem(100,~"test");	
 
     loop {
         print(CMD_PROMPT);
         let line = io::stdin().read_line();
         debug!(fmt!("line: %?", line));
-	hist[x]=line;
-x=x+1;
+
+	// Adding the line to history.
+	hist[ct] = std::str::to_owned(line);
+	ct += 1;
+
         let mut argv: ~[~str] = line.split_iter(' ').filter(|&x| x != "")
                                  .transform(|x| x.to_owned()).collect();
         debug!(fmt!("argv %?", argv));
@@ -20,16 +23,14 @@ x=x+1;
             match program {
                 ~"exit"     => {return; }
 		~"history"  => {
-			int::range(0,x){|i|
-			println(hist[i]);
-}
-		
-}
+			for std::uint::range(0,ct) |i| {
+				println(fmt!("%s  %s",std::uint::to_str(i+1),hist[i]));
+			}
+	        }
 		~"cd"	    => {println("RUST");}
                 _           => {run::process_status(program, argv);}
             }
 	    // Implement history list here.
-	    
         }
     }
 }
